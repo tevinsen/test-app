26a6f44cbd198301c5f911858c738981a2b30e2b
tevinsen <tevin@interviewme.tech>
2023-01-23 12:06:48 +0200

    update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,28 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["var(--font-poppins)"],
+    },
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      "2xl": "1400px",
+    },
+    extend: {
+      colors: {
+        transparent: "transparent",
+        "ime-dark": "#0A111C",
+        "ime-accent": "#0070F3",
+        "ime-blue": "#00C7FF",
+        "ime-purple": "#601EF9",
+        "ime-pink": "#EA3379",
+        "ime-yellow": "#F0B03D",
+        "ime-orange": "#FD563D",
+      },
+    },
   },
   plugins: [],
 };
